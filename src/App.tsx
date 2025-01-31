8e5dcf5c01f8116bc8a698d5dcb2fce355d48805
Fahad Khan <kfahad5607@gmail.com>
2025-01-31 10:16:20 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DataTable from "./components/DataTable";
+
+function App() {
+
+  return (
+    <div className="bg-stone-100 min-h-screen py-10 px-12 mx-auto font-inter">
+      <DataTable />
+    </div>
+  )
+}
+
+export default App
